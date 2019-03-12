1012903c6822994ec9e75ba1ec77f5dedf8c695a
Neo <echiang73@gmail.com>
2019-03-12 00:34:29 -0700

    working update display every second

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,25 +1,24 @@
- // Initialize Firebase
- var config = {
-    apiKey: "AIzaSyAvmZSPq4xICPs16mWbkWmbOGwmSHAAh-o",
-    authDomain: "game-of-thrones-train-schedule.firebaseapp.com",
-    databaseURL: "https://game-of-thrones-train-schedule.firebaseio.com",
-    projectId: "game-of-thrones-train-schedule",
-    storageBucket: "game-of-thrones-train-schedule.appspot.com",
-    messagingSenderId: "801680752075"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAvmZSPq4xICPs16mWbkWmbOGwmSHAAh-o",
+  authDomain: "game-of-thrones-train-schedule.firebaseapp.com",
+  databaseURL: "https://game-of-thrones-train-schedule.firebaseio.com",
+  projectId: "game-of-thrones-train-schedule",
+  storageBucket: "game-of-thrones-train-schedule.appspot.com",
+  messagingSenderId: "801680752075"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  // Functions ----------------------------------------------------------------------------
+// Functions ----------------------------------------------------------------------------
 
-  // On click button to submit new train information
-  $("#add-train-btn").on("click", function(event) {
-    event.preventDefault();
+// On click button to submit new train information
+$("#add-train-btn").on("click", function (event) {
+  event.preventDefault();
   var trainName = $("#train-name-input").val().trim();
   var destination = $("#destination-input").val().trim();
   var timeFirstTrain = moment($("#time-first-train-input").val().trim(), "HH:mm").format("HH:mm A");
-  // var timeFirstTrain = $("#time-first-train-input").val().trim();
   var frequency = $("#frequency-input").val().trim();
 
   var newInput = {
@@ -41,11 +40,11 @@
   $("#destination-input").val("");
   $("#time-first-train-input").val("");
   $("#frequency-input").val("");
-  });
+});
 
 
-// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
-database.ref().on("child_added", function(childSnapshot) {
+// Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function (childSnapshot) {
   console.log(childSnapshot.val());
 
   // Store everything into a variable.
@@ -54,7 +53,7 @@ database.ref().on("child_added", function(childSnapshot) {
   var timeFirstTrain = childSnapshot.val().timeFirstTrain;
   var frequency = childSnapshot.val().frequency;
 
-  // Employee Info
+  // Train Info
   console.log(trainName);
   console.log(destination);
   console.log("Firebase - 1st Train Time is: " + timeFirstTrain);
@@ -72,13 +71,13 @@ database.ref().on("child_added", function(childSnapshot) {
   console.log("Difference in time: " + diffTime);
 
   // Time apart (remainder)
-  var timeRemainder = diffTime%frequency;
+  var timeRemainder = diffTime % frequency;
   console.log(timeRemainder);
 
   // Minutes Until Train
-  var minutesTillTrain = frequency-timeRemainder;
+  var minutesTillTrain = frequency - timeRemainder;
   console.log("Minutes until train: " + minutesTillTrain);
-  
+
   // Next Train
   var nextTrain = moment().add(minutesTillTrain, "minutes");
   console.log("Arrival time: " + moment(nextTrain).format("hh:mm A"));
@@ -94,14 +93,39 @@ database.ref().on("child_added", function(childSnapshot) {
 
   // Append the new row to the table
   $("#schedule-display > tbody").append(newRow);
+
+}, function(errorObject) {
+  console.log("Errors handled: " + errorObject.code);
 });
 
-$("#current-time").text("Current date and time: " + moment().format("dddd, MMMM Do YYYY, hh:mm A"));
+$("#current-time").text("Current date and time: " + moment().format("dddd, MMMM Do YYYY, hh:mm:ss A"));
 
-// Example Time Math
-// -----------------------------------------------------------------------------
-// Assume Employee start date of January 1, 2015
-// Assume current date is March 1, 2016
+// function to refresh display every second
+var windowTimeout = setInterval(function () {
+  $("#current-time").text("Current date and time: " + moment().format("dddd, MMMM Do YYYY, hh:mm:ss A"));
+  $("#schedule-display > tbody").empty();
+  console.log("15 sec refresh");
+  database.ref().on("child_added", function(childSnapshot) {
+  
+  var trainName = childSnapshot.val().trainName;
+  var destination = childSnapshot.val().destination;
+  var timeFirstTrain = childSnapshot.val().timeFirstTrain;
+  var frequency = childSnapshot.val().frequency;
 
-// We know that this is 15 months.
-// Now we will create code in moment.js to confirm that any attempt we use meets this test case
+  var currentTime = (moment().format("hh:mm A"));
+  var timeFirstTrainConverted = moment(timeFirstTrain, "HH:mm").subtract(1, "years");
+  var diffTime = moment().diff(moment(timeFirstTrainConverted), "minutes");
+  var timeRemainder = diffTime % frequency;
+  var minutesTillTrain = frequency - timeRemainder;
+  var nextTrain = moment().add(minutesTillTrain, "minutes");
+
+  var newRow = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(destination),
+    $("<td>").text(frequency),
+    $("<td>").text(moment(nextTrain).format("hh:mm A")),
+    $("<td>").text(minutesTillTrain)
+  );
+  $("#schedule-display > tbody").append(newRow);
+  });
+}, 1000 * 1);
